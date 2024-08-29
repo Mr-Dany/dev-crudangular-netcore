9a80c928b803b11f404422eeed2c9305bfa134c5
Mr-Dany <49878464+Mr-Dany@users.noreply.github.com>
2024-08-28 22:00:04 -0500

    Crear Componente TarjetaCredito
    -Generamos componente mediante consola
    -Utilizamos nuestro nuevo componente tarjeta crédito
    -para poder utilizar importamos en los imports de app.component.ts

--- FETarjetaCredito/src/app/app.component.ts
+++ FETarjetaCredito/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TarjetaCreditoComponent } from './components/tarjeta-credito/tarjeta-credito.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TarjetaCreditoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
