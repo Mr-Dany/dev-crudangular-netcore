20281618adbfebc58145baee216df8b0fa655438
Mr-Dany <49878464+Mr-Dany@users.noreply.github.com>
2024-08-29 22:03:31 -0500

    Listado de tarjetas dinamico
    -Creamos un array con 2 registros
    -importamos moduLo common en imports para poder utilizar el for en el html
    -recorremos el array de tarjetas

--- FETarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ FETarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tarjeta-credito',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tarjeta-credito.component.html',
   styleUrl: './tarjeta-credito.component.css'
 })
 export class TarjetaCreditoComponent {
+  listTarjetas: any[] = [
+    { titular: 'juan perez', numeroTarjeta: '25252526262', fechaExpericion: '11/23', cvv: '123' },
+    { titular: 'miguel gonzales', numeroTarjeta: '252526262', fechaExpericion: '11/24', cvv: '321' }
+  ]
 
 }
